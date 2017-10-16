4f3d551fd58944350c871a865eeb8c97fd9b1873
Yuichi Yoshida <73.307.ry@gmail.com>
2017-10-16 14:09:06 +0000

    Add name to view

--- db/migrate/20171015165445_create_messages.rb
+++ db/migrate/20171015165445_create_messages.rb
@@ -1,9 +1,8 @@
-class CreateMessages < ActiveRecord::Migration[5.1]
+rails g migration AddNameToMessages name:stringrails g migration AddNameToMessages name:string class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
       t.string :title
       t.string :message
-
       t.timestamps
     end
   end
